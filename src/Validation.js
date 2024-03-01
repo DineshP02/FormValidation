145f2639ea8b36e3e6bdc9465bd80d2270213873
DineshP02 <152846967+DineshP02@users.noreply.github.com>
2024-03-01 17:03:04 +0530

    Update value

--- src/Validation.js
+++ src/Validation.js
@@ -2,5 +2,5 @@ import * as yup from "yup";
 export const UserValidation = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
-  password: yup.string().min(3).max(8).required(),
+  password: yup.string().min(3).max(10).required(),
 });
